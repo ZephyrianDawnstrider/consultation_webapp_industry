54ce243d0094620d41302305a64561f1c3159cd8
Ayush Chaturvedi <ayushchaturvedi405@gmail.com>
2025-06-05 17:59:59 +0530

    third commit

--- consultaion_webapp/settings.py
+++ consultaion_webapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(=4utn9oke4(8mn&$2jpaz#x=$#g^%bj^%!v=qw_b%8$1g*0)r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['consultaion-webapp.onrender.com','*']
 
 
 # Application definition
