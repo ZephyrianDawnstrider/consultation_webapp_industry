9739ea04de9fee02c12718b5456c62d851676ac2
Ayush Chaturvedi <ayushchaturvedi405@gmail.com>
2025-06-05 17:57:17 +0530

    third commit

--- consultaion_webapp/settings.py
+++ consultaion_webapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(=4utn9oke4(8mn&$2jpaz#x=$#g^%bj^%!v=qw_b%8$1g*0)r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
