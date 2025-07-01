08ed4420dda8af74e280d3cf299370aacc4c20c9
Ayush Chaturvedi <ayushchaturvedi405@gmail.com>
2025-07-01 14:11:09 +0530

    46 commit

--- consultaion_webapp/settings.py
+++ consultaion_webapp/settings.py
@@ -79,10 +79,19 @@ WSGI_APPLICATION = 'consultaion_webapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': os.getenv('MSSQL_DB_NAME', 'softechconsultant'),
+        'USER': os.getenv('MSSQL_DB_USER', 'sofcunsuser'),
+        'PASSWORD': os.getenv('MSSQL_DB_PASSWORD', 'REDACTED'),
+        'HOST': os.getenv('MSSQL_DB_HOST', 'REDACTED'),
+        'PORT': os.getenv('MSSQL_DB_PORT', ''),
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+        },
     }
 }
 
